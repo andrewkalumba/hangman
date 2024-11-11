2362a13b32ffe90dc5cc6a2a27687bb8a1237e6b
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-11 12:13:59 +0100

    first commit

---
+++ scripts/hangman.js
@@ -0,0 +1,72 @@
+//alert("Welcome");
+// Create an array of words
+var words = [
+    "javascript",
+    "monkey",
+    "amazing",
+    "pancake"
+    ];
+    // Pick a random word
+    var word = words[Math.floor(Math.random() * words.length)];
+
+    /*function validate() {
+        if (document.myForm.name.value == "") {
+            alert("Enter a name");
+            document.myForm.name.focus();
+            return false;
+        }
+        if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
+            alert("Invalid characters");
+            document.myForm.name.focus();
+            return false;
+        }
+    }*/
+
+    function allLetter(inputtxt)
+  {
+   var letters = /^[A-Za-z]+$/;
+   if(inputtxt.value.match(letters))
+     {
+      return true;
+     }
+   else
+     {
+     alert("message");
+     return false;
+     }
+  }
+
+    // Set up the answer array
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+    answerArray[i] = (`$(Hangman starts) "_"`);
+    }
+    var remainingLetters = word.length;
+
+    // The game loop
+    while (remainingLetters > 0) {
+    // Show the player their progress
+    alert(answerArray.join(" "));
+
+    // Get a guess from the player
+    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+    if (guess === null) {
+    // Exit the game loop
+    break;
+    } else if (guess.length !== 1) {
+    alert("Please enter a single letter.");
+    } else {
+    // Update the game state with the guess
+    for (var j = 0; j < word.length; j++) {
+    if (word[j] === guess) {
+    answerArray[j] = guess;
+    remainingLetters--;
+    }
+    }
+    }
+
+    // The end of the game loop
+    }
+    // Show the answer and congratulate the player
+    alert(answerArray.join(" "));
+    alert("Good job! The answer was " + word);
