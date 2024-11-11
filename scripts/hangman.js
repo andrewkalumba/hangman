f84073e44a271ed9decf362bbe6be7c66cbbe966
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-11 22:51:24 +0100

    updated sub-branch

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,72 +1,102 @@
 //alert("Welcome");
-// Create an array of words
-var words = [
-    "javascript",
-    "monkey",
-    "amazing",
-    "pancake"
-    ];
-    // Pick a random word
-    var word = words[Math.floor(Math.random() * words.length)];
 
-    /*function validate() {
-        if (document.myForm.name.value == "") {
-            alert("Enter a name");
-            document.myForm.name.focus();
-            return false;
-        }
-        if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
-            alert("Invalid characters");
-            document.myForm.name.focus();
-            return false;
-        }
-    }*/
+  var words=[ "spain", "sweden", "england", "denmark", "uganda",
+    "norway", "usa", "poland", "france", "sudan", "germany"];
 
-    function allLetter(inputtxt)
-  {
-   var letters = /^[A-Za-z]+$/;
-   if(inputtxt.value.match(letters))
-     {
-      return true;
-     }
-   else
-     {
-     alert("message");
-     return false;
-     }
-  }
-
-    // Set up the answer array
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-    answerArray[i] = (`$(Hangman starts) "_"`);
+    var word=words[Math.floor(Math.random()*words.length)];
+    var answerArray=[];
+    for (var i=0; i<word.length; i++)
+    {
+       answerArray[i] = "_";
     }
+    
+    var guessArray=[];
+    var lives=word.length;
+    lives=lives+2;
     var remainingLetters = word.length;
+    alert('lets start the game')
+    console.log('Lets start the game!\n');
+    while (remainingLetters>0)
+    {
+      alert(answerArray.join(" ")) 
+       console.log(answerArray.join(" "));
+       console.log('\n');
 
-    // The game loop
-    while (remainingLetters > 0) {
-    // Show the player their progress
+       if (lives==1)
+       {
+        alert("You have one life left")
+           console.log('You have 1 life left.');
+       }
+    
+    
+       else if(lives>1)
+       {
+          alert('You have ' + lives + ' lives left!') 
+          console.log('You have ' + lives + ' lives left.');
+       }
+    
+    
+       var guess  = prompt('Guess a letter. ');
+       console.log('\n');
+       if (guessArray.includes(guess))
+       {
+          alert('Already chosen') 
+           console.log('Already chosen. ');
+       }
+       console.log('\n');
+       guessArray.push(guess);
+       if (guess===null)
+       {
+           break;
+       }
+       else if(guess.length!==1)
+       {
+           alert('Please enter a single letter') 
+           console.log('Please enter a single letter.');
+           console.log('\n');
+       }
+    
+    
+       else if(!word.includes(guess))
+       {
+           lives--;
+           console.log('One life lost. ');
+           console.log('\n');
+       }
+    
+    
+       else if (lives==0)
+       {
+           alert('Sorry, you lost!') 
+           console.log('Oof');
+       }
+    
+       else
+       {
+           for (var j=0; j<word.length; j++)
+           {
+               if (word[j]=== guess)
+               {
+                   answerArray[j] = guess;
+                   remainingLetters--;
+               }
+           }
+       }
+    
+       if (lives==0)
+       {
+           break;
+       }
+    }
+    
+    
+    console.log('\n');
+    console.log(answerArray.join(" "));
+    console.log('The answer was ' + word + '!');
+    console.log('Bye.');
     alert(answerArray.join(" "));
-
-    // Get a guess from the player
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null) {
-    // Exit the game loop
-    break;
-    } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-    } else {
-    // Update the game state with the guess
-    for (var j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-    answerArray[j] = guess;
-    remainingLetters--;
-    }
-    }
-    }
-
-    // The end of the game loop
-    }
-    // Show the answer and congratulate the player
-    alert(answerArray.join(" "));
-    alert("Good job! The answer was " + word);
+    alert('The word was ' + word + '!');
+    alert('Bye');
+    
+    
+  
