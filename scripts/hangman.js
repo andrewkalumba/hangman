e6e935f22fb09b2a5e72b298cce96ac0d10d0791
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-11 23:36:25 +0100

    new inserts to js, html and css

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -16,63 +16,44 @@
     var remainingLetters = word.length;
     alert('lets start the game')
     console.log('Lets start the game!\n');
-    while (remainingLetters>0)
-    {
+    while (remainingLetters>0) {
       alert(answerArray.join(" ")) 
        console.log(answerArray.join(" "));
-       console.log('\n');
 
-       if (lives==1)
-       {
-        alert("You have one life left")
-           console.log('You have 1 life left.');
+       if (lives==1) {
+          alert("You have one life left")
+          console.log('You have 1 life left.');
        }
     
-    
-       else if(lives>1)
-       {
+       else if(lives>1) {
           alert('You have ' + lives + ' lives left!') 
           console.log('You have ' + lives + ' lives left.');
        }
     
-    
        var guess  = prompt('Guess a letter. ');
-       console.log('\n');
-       if (guessArray.includes(guess))
-       {
+    
+       if (guessArray.includes(guess)) {
           alert('Already chosen') 
            console.log('Already chosen. ');
        }
-       console.log('\n');
+       
        guessArray.push(guess);
-       if (guess===null)
-       {
+       if (guess===null) {
            break;
-       }
-       else if(guess.length!==1)
-       {
+
+       } else if(guess.length!==1) {
            alert('Please enter a single letter') 
            console.log('Please enter a single letter.');
-           console.log('\n');
-       }
-    
-    
-       else if(!word.includes(guess))
-       {
+          
+       } else if(!word.includes(guess)) {
            lives--;
-           console.log('One life lost. ');
-           console.log('\n');
-       }
-    
-    
-       else if (lives==0)
-       {
+           console.log('One life lost.');
+
+       } else if (lives==0) {
            alert('Sorry, you lost!') 
-           console.log('Oof');
-       }
-    
-       else
-       {
+           console.log('Sorry, you lost');
+           
+       } else {
            for (var j=0; j<word.length; j++)
            {
                if (word[j]=== guess)
@@ -89,11 +70,10 @@
        }
     }
     
-    
-    console.log('\n');
-    console.log(answerArray.join(" "));
+    //console.log(answerArray.join(" "));
     console.log('The answer was ' + word + '!');
     console.log('Bye.');
+
     alert(answerArray.join(" "));
     alert('The word was ' + word + '!');
     alert('Bye');
