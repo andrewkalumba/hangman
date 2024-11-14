39e72c7c5f1181f0e18cd43b4678b064716a6379
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-14 15:59:44 +0100

    javascript updates

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -5,91 +5,74 @@ const words = ["spain", "sweden", "england", "denmark", "uganda",
 
 const word = () => words[Math.floor(Math.random() * words.length)];
 const answerArray = [];
+const VALIDATION = /^[a-zA-Z]{1,1}$/; //checks (^)the beginning of the validation and checks all that is inside the brackets, the (1) means it should be one and the second sets a limit.
 
 let guessArray = [];
-let lives = word.length;
-lives = lives + 2;
-let remainingLetters = word.length;
+let lives = 0;
+let remainingLetters = "";
+let pickedCountry = word();
+let hasWon = false
 
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
+lives = pickedCountry.length;
+
+for (let i = 0; i < pickedCountry.length; i++) {
+  answerArray.push("_");
 }
 
 alert('Lets start the game \nGuess the country name!');
 console.log('Lets start the game!\n');
+console.log(pickedCountry)
 
-while (remainingLetters > 0) {
-  alert(answerArray.join(" "))
+while (hasWon === false) {
 
+  //alert(answerArray.join(" "));
 
-  if (lives == 1) {
-    alert("You have only one life left")
+  let guess = prompt('Guess a letter.');
 
-  }
-
-  else if (lives > 1) {
-    alert('You have ' + lives + ' lives left!');
-    
-  }
-
-  var guess = prompt('Guess a letter.');
-
-  if (guessArray.includes(guess)) {
-    alert('Already chosen')
-  }
-
-  guessArray.push(guess);
   if (guess === null) {
+    alert("Game canceled!")
     break;
+  }
 
-  } else if (guess.length !== 1) {
-    alert('Please enter a single letter')
-    console.log('Please enter a single letter.');
-
-  } else if (!word.includes(guess)) {
-    lives--;
-    alert('One life lost');
-    console.log('One life lost.');
-
-  } else if (lives == 0) {
-    alert('Oof')
-    console.log('Oof');
-
-  } else {
-    for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-        remainingLetters--;
+  if (VALIDATION.test(guess)) {
+    if (lives === 1) {
+      alert("You have only one life left")
+    }
+    else if (lives > 1) {
+      alert('You have ' + lives + ' lives left!');
+    }
+    /*if the guess exists in the word that is picked, we go into for loop*/
+    if (pickedCountry.includes(guess)) {
+      for (let i = 0; i < pickedCountry.length; i++) { // gives the same amount of letters as in the word beginning guessed
+        if (pickedCountry[i] === guess) {
+          answerArray[i] = guess;
+          console.log(i)
+        }
+      }
+      alert(answerArray.join(" "));
+      /*if answerArray = picked country, its a win and we need to quit the game or close the loop*/
+    }
+    else {
+      lives--;
+      /* check if the life is zero and we end the game and reset the game*/
+      if (lives === 0) {
+        alert(`You have lost the game!! The right one was ${pickedCountry}`)
+        break;
+      }
+      else {
+        if (guessArray.includes(guess)) {
+          alert('Already chosen')
+        }
+        else {
+          guessArray.push(guess);
+        }
       }
     }
   }
-
-  if (lives == 0) {
-    alert('Sorry. you have lost lose!!');
-    break;
+  else {
+    alert("Insert a letter and not more than one");
   }
 }
 
-//console.log(answerArray.join(" "));
-console.log('The answer was ' + word + '!');
-console.log('Bye.');
-
 alert(answerArray.join(" "));
-alert('The word was ' + word + '!');
 alert('Bye');
-
-
-/*function allLetter(inputtxt)
-{
-var letters = /^[A-Za-z]+$/;
-if(inputtxt.value.match(letters))
-{
- return true;
-}
-else
-{
-alert("message");
-return false;
-}
-}*/
-
