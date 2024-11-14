5572137dbc1b9a7dd6f97b5f1f86107ae02da592
Andrew Julius Kalumba <andrewkalumba@MacBook-Air-som-tillhor-Andrew.local>
2024-11-14 11:00:14 +0100

    new updates

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,98 +1,95 @@
 //alert("Welcome");
 
-  const words=[ "spain", "sweden", "england", "denmark", "uganda",
-    "norway", "usa", "poland", "france", "sudan", "germany"];
+const words = ["spain", "sweden", "england", "denmark", "uganda",
+  "norway", "usa", "poland", "france", "sudan", "germany"];
 
-    const word = words[Math.floor(Math.random() * words.length)];
-    const answerArray=[];
-    for (var i=0; i<word.length; i++)
-    {
-       answerArray[i] = "_";
+const word = () => words[Math.floor(Math.random() * words.length)];
+const answerArray = [];
+
+let guessArray = [];
+let lives = word.length;
+lives = lives + 2;
+let remainingLetters = word.length;
+
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+alert('Lets start the game \nGuess the country name!');
+console.log('Lets start the game!\n');
+
+while (remainingLetters > 0) {
+  alert(answerArray.join(" "))
+
+
+  if (lives == 1) {
+    alert("You have only one life left")
+
+  }
+
+  else if (lives > 1) {
+    alert('You have ' + lives + ' lives left!');
+    
+  }
+
+  var guess = prompt('Guess a letter.');
+
+  if (guessArray.includes(guess)) {
+    alert('Already chosen')
+  }
+
+  guessArray.push(guess);
+  if (guess === null) {
+    break;
+
+  } else if (guess.length !== 1) {
+    alert('Please enter a single letter')
+    console.log('Please enter a single letter.');
+
+  } else if (!word.includes(guess)) {
+    lives--;
+    alert('One life lost');
+    console.log('One life lost.');
+
+  } else if (lives == 0) {
+    alert('Oof')
+    console.log('Oof');
+
+  } else {
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
     }
-    
-    var guessArray=[];
-    var lives=word.length;
-    lives=lives+2;
-    var remainingLetters = word.length;
-    alert('Lets start the game \nGuess the country name!');
-    console.log('Lets start the game!\n');
+  }
 
-    while (remainingLetters>0) {
-      alert(answerArray.join(" ")) 
-      console.log(answerArray.join(" "));
+  if (lives == 0) {
+    alert('Sorry. you have lost lose!!');
+    break;
+  }
+}
 
-       if (lives==1) {
-          alert("You have only one life left")
-          console.log('You have only one life left.');
-       }
-    
-       else if(lives>1) {
-          alert('You have ' + lives + ' lives left!');
-          console.log('You have ' + lives + ' lives left.');
-       }
-    
-       var guess  = prompt('Guess a letter.');
-    
-       if (guessArray.includes(guess)) {
-          alert('Already chosen') 
-           console.log('Already chosen.');
-       }
-       
-       guessArray.push(guess);
-       if (guess===null) {
-           break;
+//console.log(answerArray.join(" "));
+console.log('The answer was ' + word + '!');
+console.log('Bye.');
 
-       } else if(guess.length!==1) {
-           alert('Please enter a single letter') 
-           console.log('Please enter a single letter.');
-          
-       } else if(!word.includes(guess)) {
-           lives--;
-           alert('One life lost');
-           console.log('One life lost.');
+alert(answerArray.join(" "));
+alert('The word was ' + word + '!');
+alert('Bye');
 
-       } else if (lives==0) {
-           alert('Oof') 
-           console.log('Oof');
 
-       } else {
-           for (var j=0; j<word.length; j++)
-           {
-               if (word[j]=== guess)
-               {
-                   answerArray[j] = guess;
-                   remainingLetters--;
-               }
-           }
-       }
-    
-       if (lives==0) {
-        alert('Sorry. you have lost lose!!');
-           break;
-       }
-    }
-    
-    //console.log(answerArray.join(" "));
-    console.log('The answer was ' + word + '!');
-    console.log('Bye.');
+/*function allLetter(inputtxt)
+{
+var letters = /^[A-Za-z]+$/;
+if(inputtxt.value.match(letters))
+{
+ return true;
+}
+else
+{
+alert("message");
+return false;
+}
+}*/
 
-    alert(answerArray.join(" "));
-    alert('The word was ' + word + '!');
-    alert('Bye');
-    
-
-       /*function allLetter(inputtxt)
-    {
-     var letters = /^[A-Za-z]+$/;
-     if(inputtxt.value.match(letters))
-       {
-        return true;
-       }
-     else
-       {
-       alert("message");
-       return false;
-       }
-    }*/
-    
-  
